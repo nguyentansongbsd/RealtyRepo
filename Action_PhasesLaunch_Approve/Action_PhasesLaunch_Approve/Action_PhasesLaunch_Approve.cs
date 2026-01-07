120857d23fb117c74569ed33007336dc3a78db51
nguyentansongbsd <songnt@bsdinsight.com>
2026-01-07 17:59:30 +0700

    action duyệt PL

---
+++ Action_PhasesLaunch_Approve/Action_PhasesLaunch_Approve/Action_PhasesLaunch_Approve.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Action_PhasesLaunch_Approve
+{
+    public class Action_PhasesLaunch_Approve : IPlugin
+    {
+        private IPluginExecutionContext _context;
+        private IOrganizationService _service;
+        private ITracingService _tracingService;
+        private IOrganizationServiceFactory _serviceFactory;
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            this._context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            this._tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            this._serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));    
+            this._service = _serviceFactory.CreateOrganizationService(this._context.UserId);
+
+            try
+            {
+                EntityReference enfPL = this._context.InputParameters["Target"] as EntityReference;
+
+                Entity enPL = new Entity(enfPL.LogicalName, enfPL.Id);
+                enPL["bsd_approvedate"] = RetrieveLocalTimeFromUTCTime(DateTime.UtcNow, _service);
+                enPL["bsd_approver"] = new EntityReference("systemuser", this._context.UserId);
+                enPL["statuscode"] = new OptionSetValue(100000000);
+                this._service.Update(enPL);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+        }
+        private DateTime RetrieveLocalTimeFromUTCTime(DateTime utcTime, IOrganizationService service)
+        {
+            int? timeZoneCode = RetrieveCurrentUsersSettings(service);
+            if (!timeZoneCode.HasValue)
+                throw new InvalidPluginExecutionException("Can't find time zone code");
+            var request = new LocalTimeFromUtcTimeRequest
+            {
+                TimeZoneCode = timeZoneCode.Value,
+                UtcTime = utcTime.ToUniversalTime()
+            };
+            var response = (LocalTimeFromUtcTimeResponse)service.Execute(request);
+            return response.LocalTime;
+        }
+        private int? RetrieveCurrentUsersSettings(IOrganizationService service)
+        {
+            var currentUserSettings = service.RetrieveMultiple(
+            new QueryExpression("usersettings")
+            {
+                ColumnSet = new ColumnSet("localeid", "timezonecode"),
+                Criteria = new FilterExpression
+                {
+                    Conditions = { new ConditionExpression("systemuserid", ConditionOperator.EqualUserId) }
+                }
+            }).Entities[0].ToEntity<Entity>();
+            return (int?)currentUserSettings.Attributes["timezonecode"];
+        }
+    }
+}
