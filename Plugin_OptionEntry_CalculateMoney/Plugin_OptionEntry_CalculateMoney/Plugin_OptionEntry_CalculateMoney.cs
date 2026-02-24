73f46eb353e80404e70bb742ff4ae2e4f51c8a82
Dan Nguyen <>
2026-02-24 16:41:25 +0700

    sửa ct

--- Plugin_OptionEntry_CalculateMoney/Plugin_OptionEntry_CalculateMoney/Plugin_OptionEntry_CalculateMoney.cs
+++ Plugin_OptionEntry_CalculateMoney/Plugin_OptionEntry_CalculateMoney/Plugin_OptionEntry_CalculateMoney.cs
@@ -170,30 +170,28 @@ namespace Plugin_OptionEntry_CalculateMoney
                 bsd_totalamountlessfreight = GetMoney(enOE, "bsd_totalamountlessfreight");
             else
                 bsd_totalamountlessfreight = GetMoney(enUp, "bsd_totalamountlessfreight");
-            decimal bsd_packagesellingamount = 0;
             decimal bsd_freightamount = 0;
 
             if (!enOE.Contains("bsd_quoteid") && !enOE.Contains("bsd_reservationcontract"))  // từ sp
             {
                 trace.Trace("từ sp");
 
-                bsd_packagesellingamount = GetPackageSellingAmount(enOE, bsd_detailamount);
+                decimal bsd_packagesellingamount = GetPackageSellingAmount(enOE, bsd_detailamount);
                 enUp["bsd_packagesellingamount"] = new Money(bsd_packagesellingamount);
 
-                bsd_freightamount = GetFreightAmount(enOE, bsd_detailamount);
+                bsd_freightamount = GetFreightAmount(enOE, bsd_totalamountlessfreight);
                 enUp["bsd_freightamount"] = new Money(bsd_freightamount);
 
-                SetTotalTaxAndTotalAmount(enOE, bsd_totalamountlessfreight, bsd_freightamount, bsd_packagesellingamount, ref enUp);
+                SetTotalTaxAndTotalAmount(enOE, bsd_totalamountlessfreight, bsd_freightamount, ref enUp);
             }
             else  // từ convert
             {
                 if (context.MessageName == "Update")
                 {
                     trace.Trace("từ convert");
-                    bsd_packagesellingamount = GetMoney(enOE, "bsd_packagesellingamount");
                     bsd_freightamount = GetMoney(enOE, "bsd_freightamount");
 
-                    SetTotalTaxAndTotalAmount(enOE, bsd_totalamountlessfreight, bsd_freightamount, bsd_packagesellingamount, ref enUp);
+                    SetTotalTaxAndTotalAmount(enOE, bsd_totalamountlessfreight, bsd_freightamount, ref enUp);
                 }
             }
         }
@@ -223,17 +221,17 @@ namespace Plugin_OptionEntry_CalculateMoney
             return bsd_packagesellingamount;
         }
 
-        private decimal GetFreightAmount(Entity enOE, decimal bsd_detailamount)
+        private decimal GetFreightAmount(Entity enOE, decimal bsd_totalamountlessfreight)
         {
             trace.Trace("GetFreightAmount");
 
             EntityReference refUnit = (EntityReference)enOE["bsd_unitnumber"];
             Entity enUnit = service.Retrieve(refUnit.LogicalName, refUnit.Id, new ColumnSet(new string[] { "bsd_maintenancefeespercent" }));
             decimal bsd_maintenancefeespercent = enUnit.Contains("bsd_maintenancefeespercent") ? (decimal)enUnit["bsd_maintenancefeespercent"] / 100 : 0;
-            return bsd_maintenancefeespercent * bsd_detailamount;
+            return bsd_maintenancefeespercent * bsd_totalamountlessfreight;
         }
 
-        private void SetTotalTaxAndTotalAmount(Entity enOE, decimal bsd_totalamountlessfreight, decimal bsd_freightamount, decimal bsd_packagesellingamount, ref Entity enUp)
+        private void SetTotalTaxAndTotalAmount(Entity enOE, decimal bsd_totalamountlessfreight, decimal bsd_freightamount, ref Entity enUp)
         {
             trace.Trace("SetTotalTaxAndTotalAmount");
 
@@ -246,7 +244,9 @@ namespace Plugin_OptionEntry_CalculateMoney
             }
             decimal bsd_totaltax = bsd_totalamountlessfreight * percentTax;
             enUp["bsd_totaltax"] = new Money(bsd_totaltax);
-            enUp["bsd_totalamount"] = new Money(bsd_totalamountlessfreight + bsd_totaltax + bsd_freightamount + bsd_packagesellingamount);
+            decimal bsd_totalamountlessfreightaftervat = bsd_totalamountlessfreight + bsd_totaltax;
+            enUp["bsd_totalamountlessfreightaftervat"] = new Money(bsd_totalamountlessfreightaftervat);
+            enUp["bsd_totalamount"] = new Money(bsd_totalamountlessfreightaftervat + bsd_freightamount);
         }
 
         private decimal GetUnitPrice(Entity enTarget, Entity target, ref Entity enUp)
